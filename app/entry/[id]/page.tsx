1aa5b87ecced9a521da6f2d5222639d23c2a9783
Chyisiang <chyisiang8@gmail.com>
2024-02-13 17:44:26 +0800

    remove unneeded code

--- app/entry/[id]/page.tsx
+++ app/entry/[id]/page.tsx
@@ -1,54 +1,15 @@
-// "use client";
-// import { useEffect, useState } from "react";
-
 async function getEntry(id: string) {
-  console.log(id)
+  console.log(id);
   const res = await fetch(`http://localhost:3000/api/entry/${id}`);
   const data = await res.json();
   return data;
 }
 
 async function Entry({ params }: { params: { id: string } }) {
-  // const [entry, setEntry] = useState(null);
-  // const [showDialog, setShowDialog] = useState(false);
-  // const [formData, setFormData] = useState({
-  //   name: "",
-  //   phoneNo: "",
-  //   email: "",
-  // });
-
-  // useEffect(() => {
-  //   async function fetchEntry() {
-  //     const response = await fetch(`/api/entry/${params.id}`);
-  //     const data = await response.json();
-  //     setEntry(data);
-  //   }
-
-  //   fetchEntry();
-  // }, [params.id]);
-
-  // const handleUpdate = async () => {
-  //   const response = await fetch(`/api/entry/${params.id}`, {
-  //     method: 'PUT',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify(formData),
-  //   });
-  //   if (response.ok) {
-  //     const updatedEntry = await response.json();
-  //     setEntry(updatedEntry);
-  //     setShowDialog(false);
-  //   } else {
-  //     console.log("error")
-  //   }
-  // };
-
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setFormData({ ...formData, [e.target.name]: e.target.value });
-  // };
   const entry = await getEntry(params.id);
-  console.log(entry)
-  console.log(entry.id)
-  console.log(entry.name)
+  console.log(entry);
+  console.log(entry.id);
+  console.log(entry.name);
 
   return entry ? (
     <div>
@@ -56,37 +17,6 @@ async function Entry({ params }: { params: { id: string } }) {
       <p>{entry.name}</p>
       <p>{entry.phoneNo}</p>
       <p>{entry.email}</p>
-      {/* <button onClick={() => setShowDialog(true)}>Update</button>
-      {showDialog && (
-        <div>
-          <label>
-            Name:
-            <input
-              name="name"
-              value={formData.name}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            Phone Number:
-            <input
-              name="phoneNo"
-              value={formData.phoneNo}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-            />
-          </label>
-          <button onClick={handleUpdate}>Submit</button>
-          <button onClick={() => setShowDialog(false)}>Cancel</button>
-        </div>
-      )} */}
     </div>
   ) : (
     <div>Loading...</div>
