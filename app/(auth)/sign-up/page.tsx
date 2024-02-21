2cae8acd29d0d5507b0da1617a4fec52d32fa6fc
Chyisiang <chyisiang8@gmail.com>
2024-02-21 19:55:31 +0800

    sign up page

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from "@/components/form/SignUpForm";
+
+function Page() {
+  return (
+    <div className="w-full">
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default Page;
