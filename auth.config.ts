39733390905e025dd7febc34511050ace9836b50
Chyisiang <chyisiang8@gmail.com>
2024-02-24 20:57:22 +0800

    stupid workaround

--- auth.config.ts
+++ auth.config.ts
@@ -11,15 +11,17 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
 
-      const isOnHome = nextUrl.pathname.startsWith("/");
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      const isOnNew = nextUrl.pathname.startsWith("/new");
+      const isOnEntry = nextUrl.pathname.startsWith("/entry");
 
-      if (isOnHome) {
+      if (isOnDashboard || isOnNew || isOnEntry) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
         const isOnAuth =
           nextUrl.pathname === "/login" || nextUrl.pathname === "/signup";
-        if (isOnAuth) return Response.redirect(new URL("/", nextUrl));
+        if (isOnAuth) return Response.redirect(new URL("/dashboard", nextUrl));
         return true;
       }
       return true;
