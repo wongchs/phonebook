a757005e29dff3b9ff4e9786b788697ddff6ed70
Chyisiang <chyisiang8@gmail.com>
2024-02-28 20:35:27 +0800

    implement shadcn toast

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,13 +1,33 @@
+"use client";
+
 import { deleteEntry } from "@/app/actions/entry";
+import { useToast } from "./ui/use-toast";
 
 function DeleteButton({ id }: { id: string }) {
   const deleteEntryWithId = deleteEntry.bind(null, id);
-  // console.log(id);
-  // console.log(deleteEntryWithId);
+  const { toast } = useToast();
+
+  const handleDelete = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      await deleteEntryWithId();
+      toast({
+        title: "Success",
+        description: "Entry deleted successfully",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Failed to delete entry",
+      });
+    }
+  };
 
   return (
-    <form action={deleteEntryWithId}>
-      <button className="bg-red-600 p-2 rounded">Delete</button>
+    <form onSubmit={handleDelete}>
+      <button type="submit" className="bg-red-600 p-2 rounded">
+        Delete
+      </button>
     </form>
   );
 }
