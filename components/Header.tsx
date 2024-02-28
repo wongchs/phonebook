d273c47d6c711df9128237e78214558dbc8f0cb5
Chyisiang <chyisiang8@gmail.com>
2024-02-28 21:48:14 +0800

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,27 +3,28 @@
 import { logout } from "@/app/actions/auth";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Header = () => {
   const { data: session, status } = useSession();
-  console.log(session)
+  console.log(session);
 
   return (
     <header className="flex justify-between items-center px-8 py-2">
       <h1 className="text-2xl">Entries</h1>
-      <p>
-        Signed in as&nbsp;
-        {status === "authenticated" ? session?.user?.name : "..."}
-      </p>
-      <Link href="/new">New</Link>
-      <form action={logout}>
-        <button
-          disabled={status === "loading" ? true : false}
-          className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 disabled:bg-slate-50 disabled:text-slate-500"
-        >
-          Sign Out {status === "loading" ? "..." : ""}
-        </button>
-      </form>
+      <div className="flex flex-row items-center gap-2">
+        <Link href="/new">
+          <Button>New</Button>
+        </Link>
+        <form action={logout}>
+          <button
+            disabled={status === "loading" ? true : false}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 disabled:bg-slate-50 disabled:text-slate-500"
+          >
+            Sign Out {status === "loading" ? "..." : ""}
+          </button>
+        </form>
+      </div>
     </header>
   );
 };
