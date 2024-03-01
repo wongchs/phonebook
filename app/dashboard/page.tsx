ccc488a3eeed0149a3dd9426eb370c952768e16e
Chyisiang <chyisiang8@gmail.com>
2024-03-01 14:12:46 +0800

    use supabase

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { EntryList } from "@/components/EntryList";
 import Header from "@/components/Header";
 import { auth } from "@/auth";
 import { getEntries } from "../actions/entry";
+import { Button } from "@/components/ui/button";
 
 export default async function Dashboard() {
   const session = await auth();
@@ -23,17 +24,24 @@ export default async function Dashboard() {
   return (
     <>
       <Header />
-      <div className="px-8 py-4 bg-gray-100">
-        {Object.entries(groupedEntries).map(([letter, entries]) => (
-          <div key={letter} className="mb-4">
-            <h2 className="text-2xl font-bold mb-2">{letter}</h2>
-            <ul>
-              {entries.map((entry) => (
-                <EntryList key={entry.id} {...entry} />
-              ))}
-            </ul>
+      <div className="px-8 py-4">
+        {Object.keys(groupedEntries).length > 0 ? (
+          Object.entries(groupedEntries).map(([letter, entries]) => (
+            <div key={letter} className="mb-4">
+              <h2 className="text-2xl font-bold mb-2">{letter}</h2>
+              <ul>
+                {entries.map((entry) => (
+                  <EntryList key={entry.id} {...entry} />
+                ))}
+              </ul>
+            </div>
+          ))
+        ) : (
+          <div className="flex justify-center items-center mt-8 flex-col gap-6">
+            <h1 className="text-2xl font-bold">You Do Not Have Any Contacts.</h1>
+            <Button className="px-4 py-6 rounded-md animate-bounce bg-sky-600 hover:bg-sky-800">Create New Contact</Button>
           </div>
-        ))}
+        )}
       </div>
     </>
   );
---
+++ prisma/migrations/20240301055838_init_supabase/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "Entry" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phoneNo" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "Entry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Entry" ADD CONSTRAINT "Entry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("email") ON DELETE SET NULL ON UPDATE CASCADE;
